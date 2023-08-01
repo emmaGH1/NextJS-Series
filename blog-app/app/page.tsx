786fd4ad11b22f6e0e98909532a3aaff337dcb2b
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2023-08-01 15:21:53 +0100

    feat: Minor Updates

--- blog-app/app/page.tsx
+++ blog-app/app/page.tsx
@@ -1,13 +1,15 @@
+import Posts from "./components/Posts"
 
 export default function Home() {
   return (
     <main className="px-6 mx-auto">
-       <p className="mt-12 mb-12 text-4xxl text-center dark:text-white bg-no-repeat">
+       <p className="mt-12 mb-12 text-center bg-no-repeat text-4xxl dark:text-white">
          Hello and Welcome 👋&nbsp;
          <span className="whitespace-nowrap">
            I'm <span className="font-bold">Emma</span>
          </span>
        </p>
+       <Posts />
     </main>
   )
 }
