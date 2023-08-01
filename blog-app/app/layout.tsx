56091c1cb9437902e5d34d1fda2a1a6bfc47adf9
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2023-08-01 13:14:11 +0100

    feat: Add profile picture component

--- blog-app/app/layout.tsx
+++ blog-app/app/layout.tsx
@@ -1,8 +1,7 @@
 import './globals.css'
+import Navbar from './components/Navbar'
+import MyProfilePic from './components/MyProfilePic'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className='dark:bg-slate-800'>
+        <Navbar />
+        <MyProfilePic />
+        {children}
+      </body> 
     </html>
   )
 }
